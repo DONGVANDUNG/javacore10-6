1401db2af30609607870128cff64fd3fa46373c0
van dung <vandung030701@gmail.com>
2020-07-06 16:45:32 +0700

    DictionaryManagement

--- src/javacore/buoi05/bai53/Dictionary.java
+++ src/javacore/buoi05/bai53/Dictionary.java
@@ -4,6 +4,7 @@ import javacore.buoi05.bai53.Word;
 
 public class Dictionary {
     private Word[] words;
+
     public Dictionary(Word[] words) {
         this.words = words;
     }
@@ -11,28 +12,30 @@ public class Dictionary {
     public Dictionary() {
     }
 
-    public Word Search(String key, String type){
-        for (int i = 0; i <words.length ; i++) {
-            if(words[i].getKey().equalsIgnoreCase(key) && words[i].getType().equalsIgnoreCase(type)){
+    public Word Search(String key, String type) {
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].getKey().equalsIgnoreCase(key) && words[i].getType().equalsIgnoreCase(type)) {
                 return words[i];
             }
         }
         return null;
     }
-    public int countAV(){
-        int dem=0;
-        for (Word word:words) {
-            if(word.getType().equalsIgnoreCase("ANH-VIET")){
-                dem+=1;
+
+    public int countAV() {
+        int dem = 0;
+        for (Word word : words) {
+            if (word.getType().equalsIgnoreCase("ANH-VIET")) {
+                dem += 1;
             }
         }
         return dem;
     }
-    public int countVA(){
-        int dem=0;
-        for(Word word:words){
-            if(word.getType().equalsIgnoreCase("VIET-ANH")){
-                dem+=1;
+
+    public int countVA() {
+        int dem = 0;
+        for (Word word : words) {
+            if (word.getType().equalsIgnoreCase("VIET-ANH")) {
+                dem += 1;
             }
         }
         return dem;
