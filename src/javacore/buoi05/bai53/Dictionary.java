3f5dc10a69f957d74c917ab8f8d6e75b583bb592
van dung <vandung030701@gmail.com>
2020-07-06 15:01:30 +0700

    ABC

--- src/javacore/buoi05/bai53/Dictionary.java
+++ src/javacore/buoi05/bai53/Dictionary.java
@@ -1,44 +1,40 @@
 package javacore.buoi05.bai53;
 
-import java.util.Scanner;
+import javacore.buoi05.bai53.Word;
 
 public class Dictionary {
     private Word[] words;
-    Scanner sc = new Scanner(System.in);
-    public Dictionary(int n) {
-        System.out.println("Nhap vao danh sach tu dien: ");
-        for (int i = 0; i < n; i++) {
-            System.out.println("Tu thuoc loai tu dien nao: ");
-            String type = sc.nextLine();
-            if (type.equalsIgnoreCase("VA")) {
-                System.out.println("Moi ban nhap thong tin cho tu dien VIET-ANH: ");
-                System.out.println("Nhap vao tu: ");
-                String key = sc.nextLine();
-                System.out.println("Nghia cua tu: ");
-                String value = sc.nextLine();
-                sc.nextLine();
-                words[i] = new Word(key, value, type);
-            } else if (type.equalsIgnoreCase("AV")) {
-                System.out.println("Moi ban nhap thong tin cho tu dien ANH-VIET: ");
-                System.out.println("Nhap vao tu :");
-                String key = sc.nextLine();
-                System.out.println("Nghia cua tu: ");
-                String value = sc.nextLine();
-                sc.nextLine();
-                words[i] = new Word(key, value, type);
-            }
-        }
-        sc.close();
+    public Dictionary(Word[] words) {
+        this.words = words;
     }
-    public Word Search(int n){
-        System.out.println("Nhap vao tu ma ban muon tra cuu: ");
-        String word=sc.nextLine();
-        for (int i = 1; i <n ; i++) {
-            if(word.equalsIgnoreCase(words[i].getType())){
-                if(word.equalsIgnoreCase(words[i].getKey())){
 
-                }
+    public Dictionary() {
+    }
+
+    public Word Search(String key, String type){
+        for (int i = 0; i <words.length ; i++) {
+            if(words[i].getKey().equalsIgnoreCase(key) && words[i].getType().equalsIgnoreCase(type)){
+                return words[i];
             }
         }
+        return null;
+    }
+    public int countAV(){
+        int dem=0;
+        for (Word word:words) {
+            if(word.getType().equalsIgnoreCase("ANH-VIET")){
+                dem+=1;
+            }
+        }
+        return dem;
+    }
+    public int countVA(){
+        int dem=0;
+        for(Word word:words){
+            if(word.getType().equalsIgnoreCase("VIET-ANH")){
+                dem+=1;
+            }
+        }
+        return dem;
     }
 }
--- src/javacore/buoi05/bai53/Test.java
+++ src/javacore/buoi05/bai53/Test.java
@@ -1,4 +1,31 @@
 package javacore.buoi05.bai53;
 
+import java.util.Scanner;
+
 public class Test {
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap vao so luong tu: ");
+        int n=sc.nextInt();
+        sc.nextLine();
+        Word[] words = new Word[n];
+        for (int i = 0; i <n ; i++) {
+            System.out.println("Nhap ANH-VIET/VIET-ANH");
+            String type=sc.nextLine();
+            System.out.println("Nhap thong tin cho tu thu "+(i+1));
+            System.out.println("Tu do la: ");
+            String key=sc.nextLine();
+            System.out.println("Nghia cua tu: ");
+            String value=sc.nextLine();
+            words[i]=new Word(type,key,value);
+        }
+        System.out.println("Nhap vao tu ban muon tra cuu: ");
+        String key=sc.nextLine();
+        System.out.println("Tu"+key+"thuoc loai tu dien nao: ");
+        String type=sc.nextLine();
+        Dictionary dictionary = new Dictionary();
+        dictionary.Search(key,type);
+        System.out.println("So tu thuoc tu dien ANH VIET la: "+dictionary.countAV());
+        System.out.println("So tu thuoc tu dien VIET ANH la: "+dictionary.countVA());
+    }
+    }
